bf57e3c6c79af9ee77cc768b364d36cf0a76fa3f
Jasien Elimy <jasien_elimy@hotmail.co.uk>
2016-10-23 01:10:35 +0100

    Added more camera control functionality

--- Kingdoms of Kaledonia/Assets/Scripts/GameHelper.cs
+++ Kingdoms of Kaledonia/Assets/Scripts/GameHelper.cs
@@ -7,13 +7,14 @@ namespace GameHelper{
 		//================================================================================================
 		//[RTS Camera Variables]//
 		//================================================================================================
-		public static float CAMERA_MOVEMENT_SPEED = 80f;
-		public static float CAMERA_ROTATION_SPEED = 80f;
-		public static float CAMERA_SCROLL_ZOOM_SPEED = 5f;
+		public static float CAMERA_MOVEMENT_SPEED = 60f;
+		public static float CAMERA_MOVEMENT_SPEED_SHIFT_MODIFIER = 120f;
+		public static float CAMERA_ROTATION_SPEED = 90f;
+		public static float CAMERA_SCROLL_ZOOM_SPEED = 200f;
 
 		public static float SCREEN_EDGE_WIDTH_FOR_CAMERA_MOVEMENT = 10f;
-		public static float CAMERA_MAX_HEIGHT = 300f;
-		public static float CAMERA_MIN_HEIGHT = 30f;
+		public static float CAMERA_MAX_HEIGHT = 50f;
+		public static float CAMERA_MIN_HEIGHT = 10f;
 		//================================================================================================
 
 	}
